7e15126f5006baef0660e361ea030064942d86c4
aunnnn <aun.wirawit@gmail.com>
2017-03-11 00:44:06 +0700

    Initial server.

---
+++ server/boot/admin.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function(app) {
+  var User = app.models.User;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+  var password = process.env.ADMIN_PASSWORD;
+
+  User.destroyAll(function(err, res) {
+    if (err) throw err;
+    console.log('Total ' + res.count + ' user(s) are deleted.');
+    Role.destroyAll(function(err, res) {
+      if (err) throw err;
+      console.log('Total ' + res.count + ' role(s) are deleted.');
+      User.create([
+        {
+          username: 'aunnnn',
+          email: 'aun.rueopas@gmail.com',
+          password: password,
+        },
+      ], function(err, users) {
+        if (!err) {
+          console.log('--> Admin is created.');
+          //create the admin role
+          Role.create({
+            name: 'admin',
+          }, function(err, role) {
+            if (err) throw err;
+
+            role.principals.create({
+              principalType: RoleMapping.USER,
+              principalId: users[0].id,
+            }, function(err, principal) {
+              if (err) throw err;
+              console.log('--> Admin role is assigned.');
+            });
+          });
+        } else {
+          console.log('--> Cannot create admin: ' + err);
+        }
+      });
+    });
+  });
+};
---
+++ server/models/post.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(Post) {
+  // Post.greet = function(msg, cbl) {
+  //   cbl(null, 'Greetings...' + msg);
+  // };
+
+  // Post.remoteMethod('greet', {
+  //   accepts: {arg: 'msg', type: 'string'},
+  //   returns: {arg: 'greeting', type: 'string'},
+  // });
+};
