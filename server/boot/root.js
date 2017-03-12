0099fdc62425535c0aa7915f26d2fd0dce4e25a2
aunnnn <aun.wirawit@gmail.com>
2017-03-12 15:28:58 +0700

    Change first page.

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,8 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', function(req, res) {
+    res.send('Hi there...');
+  });
   server.use(router);
 };
